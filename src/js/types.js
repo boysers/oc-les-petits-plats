31fd361d4ba78d9c7dc09b2006db4df9c4d0042c
boysers <boyser2015@gmail.com>
2023-10-22 07:59:47 +0200

    fix(recipes data): quantity number type and remove unite key

---
+++ src/js/types.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * @typedef {Object} TRecipe
+ * @property {number} id
+ * @property {string} image
+ * @property {string} name
+ * @property {number} servings
+ * @property {Array<TIngredient>} ingredients
+ * @property {number} time
+ * @property {string} description
+ * @property {string} appliance
+ * @property {Array<string>} ustensils
+ */
+
+/**
+ * @typedef {Object} TIngredient
+ * @property {string} ingredient
+ * @property {number} [quantity]
+ * @property {string} [unit]
+ */
+
+export default {};
